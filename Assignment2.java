5334c88c9f8f0642af5afc4e1ec1c597bd6b984a
CalvinBlack123 <81723593+CalvinBlack123@users.noreply.github.com>
2023-02-06 23:34:22 -0500

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment2;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class Assignment2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
